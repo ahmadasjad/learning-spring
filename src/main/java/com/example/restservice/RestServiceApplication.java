c3007ba9c45223aedff4dd01347d651a90a93d3d
Ahmad Asjad <ahmad.asjad@lavu.com>
2021-06-17 13:52:04 +0530

    application entry point

---
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@Configuration
+//@EnableJpaRepositories("com.example.restservice")
+@EnableJpaRepositories("com.example.restservice.repository")
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
